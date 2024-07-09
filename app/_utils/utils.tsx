5bdba86410fb7ee59b4573ba7e1bbec02ac451b2
Thevmin Sanhinda <thevmin.s@intervest.lk>
2024-07-09 12:31:38 +0530

    fixed regular expresshion in utils

--- app/_utils/utils.tsx
+++ app/_utils/utils.tsx
@@ -1,5 +1,5 @@
 export function getYouTubeId(url:string){
-    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
-    var match = url.match(regExp);
+    const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+    let match = RegExp(regExp).exec(url);
     return (match&&match[7].length==11)? match[7] : false;
 }
